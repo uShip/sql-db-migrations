7566e9d3be628a9a45a1aef3fff7edec47d73bfe
srujanreddyj <jsrujanreddy@gmail>
2023-11-13 15:56:56 -0600

    loading first set of table creation scripts

--- sql/Pricing/dbo/R__dbo_bb_v6.sql
+++ sql/Pricing/dbo/R__dbo_bb_v6.sql
@@ -1,44 +1,58 @@
-{# SET DEADLOCK_PRIORITY LOW
+SET DEADLOCK_PRIORITY LOW
 SET LOCK_TIMEOUT 20000
 
 IF
-( NOT EXISTS
-(select object_id from sys.objects where object_id = OBJECT_ID(N'[dbo].[bb_v6]') and type = 'U')
+    (
+        NOT EXISTS
+        (
+            SELECT object_id
+            FROM sys.objects
+            WHERE object_id = OBJECT_ID(N'[dbo].[bb_v6]') AND type = 'U'
+        )
+    )
+    BEGIN
+
+        CREATE TABLE dbo.bb_v6 (
+            bb_rating [int] NOT NULL,
+            bb_surcharged [float] NULL,
+            bb_surchargep [float] NULL,
+            datecreatedutc datetime2 NOT NULL CONSTRAINT df_bigandbulkysurcharge_datecreated DEFAULT (
+                SYSUTCDATETIME()
+            ),
+            dateupdatedutc datetime2 NOT NULL CONSTRAINT df_bigandbulkysurcharge_dateupdated DEFAULT (
+                SYSUTCDATETIME()
+            )
+        ) ON [PRIMARY]
+
+    END
+
+CREATE TABLE #bb_v6_insert (
+    bb_rating int, bb_surcharged float, bb_surchargep float
 )
-BEGIN
-
-CREATE TABLE dbo.bb_v6(
-	[bb_rating] [int] NOT NULL,
-	[bb_surchargeD] [float] NULL,
-	[bb_surchargeP] [float] NULL,
-	[DateCreatedUTC] datetime2 NOT NULL CONSTRAINT [DF_BigAndBulkySurcharge_DateCreated]  DEFAULT (sysutcdatetime()),
-	[DateUpdatedUTC] datetime2 NOT NULL CONSTRAINT [DF_BigAndBulkySurcharge_DateUpdated]  DEFAULT (sysutcdatetime())
-) ON [PRIMARY]
-
-END
-
-CREATE TABLE #bb_v6_insert ([bb_rating] int, [bb_surchargeD] float, [bb_surchargeP] float)
-INSERT INTO #bb_v6_insert ([bb_rating], [bb_surchargeD], [bb_surchargeP])
-VALUES  (0, 0, 0),
-		(1, 0, 0.1),
-		(2, 0, 0.18)
+INSERT INTO #bb_v6_insert (bb_rating, bb_surcharged, bb_surchargep)
+VALUES (0, 0, 0),
+(1, 0, 0.1),
+(2, 0, 0.18)
 
 MERGE dbo.bb_v6 AS b
-	USING #bb_v6_insert AS i
-	ON i.bb_rating = b.bb_rating
-	WHEN MATCHED THEN
-	  UPDATE SET
-            b.bb_surchargeD=i.bb_surchargeD,
-            b.bb_surchargeP=i.bb_surchargeP,
-			b.DateUpdatedUTC=sysutcdatetime()
-	WHEN NOT MATCHED THEN
-	  INSERT (
-            bb_rating,
-            bb_surchargeD,
-            bb_surchargeP
-            )
-		VALUES (
-                i.bb_rating,
-                i.bb_surchargeD,
-                i.bb_surchargeP
-                ); #}
+USING #bb_v6_insert AS i
+    ON i.bb_rating = b.bb_rating
+WHEN MATCHED
+    THEN
+    UPDATE
+        SET
+            b.bb_surcharged = i.bb_surcharged,
+            b.bb_surchargep = i.bb_surchargep,
+            b.dateupdatedutc = SYSUTCDATETIME()
+WHEN NOT MATCHED
+    THEN
+    INSERT (
+        bb_rating,
+        bb_surcharged,
+        bb_surchargep
+    )
+    VALUES (
+        i.bb_rating,
+        i.bb_surcharged,
+        i.bb_surchargep
+    );
