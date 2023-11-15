7e85e54e3b6499e6ebe6e5623a5209464fdde330
srujanreddyj <jsrujanreddy@gmail>
2023-11-15 12:32:34 -0600

    workflow to get files from python

--- get_files.py
+++ get_files.py
@@ -1,8 +1,15 @@
 import os
+import glob
 
 # Assuming your files are in the root directory of your repo
 repo_path = os.getcwd()
 
-for root, dirs, files in os.walk(repo_path):
-    for file in files:
-        print(os.path.join(root, file))
+# for root, dirs, files in os.walk(repo_path):
+#     for file in files:
+#         print(os.path.join(root, file))
+
+def find_sql_files(start_path):
+    """Recursively find all .sql files in the given directory."""
+    return glob.glob(start_path + '/**/*.sql', recursive=True)
+
+print(sorted(find_sql_files(repo_path)))
