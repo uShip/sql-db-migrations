b56cbc86878091c3b11bbfa58c3c77b4c494d389
srujanreddyj <jsrujanreddy@gmail>
2023-11-15 14:22:36 -0600

    workflow to get files from python

--- create_tables.py
+++ create_tables.py
@@ -3,6 +3,42 @@ import pyodbc
 import glob
 from datetime import datetime
 
+def connect_db(host_server, dbName, userName, userPassword) -> pyodbc.Connection:
+    """
+    Connect to database
+
+    Parameters:
+        host_server (str) = the host server name or IP address.
+        dbName (str) = the database name.
+        userName (str) = the username of login .
+        userPassword (str) = the user password for login.
+
+    Returns:
+        conn, crs = the key-value pair of the database conncection.
+    """
+
+    log_message("Establishing mssql database connection")
+    CONNECTION_STRING: str = "DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=no"
+    connection_str = CONNECTION_STRING.format(
+        server=host_server, database=dbName, username=userName, password=userPassword
+    )
+
+    log_message("Trying to connect to Database")
+    try:
+        conn = pyodbc.connect(connection_str, timeout=90)
+        # crs = conn.cursor()
+        log_message("Connected to Database")
+        return conn
+    except (pyodbc.Error, pyodbc.OperationalError) as e:
+        log_message("Failed to connect to the Database: %s", e)
+        raise Exception("Database connection timed out or failed") from e
+
+def DestroyDBConnections(conn, crs):
+    if "Connection" in str(type(conn)) and "Cursor" in str(type(crs)):
+        crs.close()
+        conn.close()
+        logger.info("Closing the connection.")
+
 def find_sql_files(start_path):
     """Recursively find all .sql files in the given directory."""
     return glob.glob(start_path + '/**/*.sql', recursive=True)
@@ -13,13 +49,14 @@ def log_message(message):
 
 def main(db_server, db_name, username, password, repo_path):
     # Create connection string
-    conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={db_server};DATABASE={db_name};UID={username};PWD={password}'
-
+    # conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={db_server};DATABASE={db_name};UID={username};PWD={password}'
+    conn, crs = connect_db(db_server, db_name, username, password)
     # Get list of .sql files in specified directory, sorted alphabetically
     sql_files = sorted(find_sql_files(repo_path))
     sql_script = "SELECT * FROM dbo.calendar"
+
     try:
-        with pyodbc.connect(conn_str) as conn:
+        with conn:
             # with open(sql_file, 'r') as file:
                 # sql_script = file.read()
             with conn.cursor() as cursor:
