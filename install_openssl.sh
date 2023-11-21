4c5887080878d271c987b41dfc2180b389554beb
srujanreddyj <jsrujanreddy@gmail>
2023-11-21 09:29:45 -0600

    adding new deployment actions to multi-env, ci-main GAs

---
+++ install_openssl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Installing OpenSSL
+wget https://www.openssl.org/source/openssl-1.1.1p.tar.gz -O openssl-1.1.1p.tar.gz
+tar -zxvf openssl-1.1.1p.tar.gz
+cd openssl-1.1.1p
+./config
+make
+make install
+ldconfig
