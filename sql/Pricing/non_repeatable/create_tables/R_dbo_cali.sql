e34fefb2b2219ad6e6d337410a19f66c0ec66f70
srujanreddyj <jsrujanreddy@gmail>
2023-11-28 12:19:40 -0600

    testing demo

--- sql/Pricing/non_repeatable/create_tables/R_dbo_cali.sql
+++ sql/Pricing/non_repeatable/create_tables/R_dbo_cali.sql
@@ -12,7 +12,7 @@ IF
     )
     BEGIN
 
-        CREATE TABLE dbo.callother3 (
+        CREATE TABLE dbo.callother2 (
             mmdd [varchar](5) NULL,
             day_date [datetime] NULL,
             day_week [float] NULL,
