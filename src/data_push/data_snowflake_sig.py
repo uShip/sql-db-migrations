0f42589ec7551295940fba20d10e326b95b9af17
srujanreddyj <jsrujanreddy@gmail>
2023-12-06 13:59:59 -0600

    testing snowflake-sig

--- src/data_push/data_snowflake_sig.py
+++ src/data_push/data_snowflake_sig.py
@@ -110,9 +110,9 @@ def main():
             if "fuelprices" in snowflake_tables[i].lower():
                 logger.info('Getting data from the snowflake table: ', snowflake_tables[i])
                 snowflake_query = f"SELECT \
-                                        DATE, \
-                                        MAX(CASE WHEN TYPE = 'Total Gasoline' THEN PPG ELSE NULL END) AS gas, \
-                                        MAX(CASE WHEN TYPE = 'No 2 Diesel' THEN PPG ELSE NULL END) AS diesel \
+                                        DATE as 'date, \
+                                        MAX(CASE WHEN TYPE = 'Total Gasoline' THEN PPG ELSE NULL END) AS 'gas', \
+                                        MAX(CASE WHEN TYPE = 'No 2 Diesel' THEN PPG ELSE NULL END) AS 'diesel' \
                                     FROM {snowflake_tables[i]} \
                                     WHERE DATE > DATEADD(DAY, -7, GETDATE()) \
                                     GROUP BY DATE;"
