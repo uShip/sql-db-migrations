7e3f98e86692654b264accca099ad8538787e61a
Srujan Jabbireddy <sjabbireddy@uship.com>
2023-12-13 14:50:18 -0600

    DEMO-12-13 (#19)
    data team demo

--- sql/Pricing/non_repeatable/create_tables/demo_file.sql
+++ sql/Pricing/non_repeatable/create_tables/demo_file.sql
@@ -12,7 +12,7 @@ IF
     )
     BEGIN
 
-        CREATE TABLE dbo.callother18 (
+        CREATE TABLE dbo.callother19 (
             mmdd [varchar](5) NULL,
             day_date [datetime] NULL,
             day_week [float] NULL,
